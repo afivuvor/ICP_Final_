6354f25306565846de963cb10184bedb7c7331df
afivuvor <104642743+afivuvor@users.noreply.github.com>
2022-11-30 02:04:26 +0000

    Update Airport.h

--- Airport.h
+++ Airport.h
@@ -1,37 +1,53 @@
+//
+// Created by Sena Vuvor on 23/11/2022.
+//
 
+#ifndef UNTITLED3_AIRPORTCPP_H
+#define UNTITLED3_AIRPORTCPP_H
 
-#ifndef C___INDIVIDUAL_PROJECT_AIRPORT_H
-#define C___INDIVIDUAL_PROJECT_AIRPORT_H
+#endif //UNTITLED3_AIRPORTCPP_H
 
 #include <string>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
-
 class Airport {
-private:
-    string airportID, name, city, country, iataCode, icaoCode;
-    double latitude, longitude;
 public:
-    static unordered_map<string, vector<Airport>> airports;
-    static unordered_map<string, Airport> codes;
-    Airport(string id, string name, string city, string country, string iataCode, string icaoCode, double latitude, double longitude);
-    static void readFile(string csvFile);
-    static vector<Airport> getAirport(string source);
-    static Airport getObject(string iataCode);
-    string getID();
-    string getName();
+    string airportID;
+    string airportName;
+    string airportCity;
+    string country;
+    string IATA;
+    string ICAO;
+    double longitude;
+    double latitude;
+
+    static unordered_map<string, vector<Airport>> locationtoAirportsMap;
+    static unordered_map<string, Airport> airportsMap;
+
+    Airport(string ID, string name, string city, string country, string IATA, string ICAO, double airlong, double lat);
+
+    /* Method declarations */
+    string toString();
+    string getAirportID();
+    void setAirportID(string AirportID);
+    void setIATA(string IATA);
+    string getIATA() ;
+    void setICAO(string ICAO);
+    string getICAO() ;
+    string getAirportCity();
+    void setAirportCity(string airportCity);
+    string getAirportName();
+    void setAirportName(string airportName);
     string getCountry();
-    string getIataCode();
-    string getIcaoCode();
-    string getCity();
-    double getLatitude();
+    void setCountry(string country);
     double getLongitude();
+    void setLongitude(double longitude);
+    double getLatitude();
+    void setLatitude(double latitude);
 
-
-
+    static void readAirportFile(string airportFile);
+    static Airport getObject(string IATA);
+    static vector<Airport> getAirport(string source);
 };
-
-
-#endif //C___INDIVIDUAL_PROJECT_AIRPORT_H
